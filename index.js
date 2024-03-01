a17e15871aa48300a6bd6bd13fa39c6f85414ffe
bonecahStefanie <stefaniesilvadeoliveira@yahoo.com.br>
2024-03-01 10:46:22 -0300

    atualizações

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors());
 
-const orders = []
+const orders = [];
 
 const checkOrderId = (request, response, next) =>{
     const {id} = request.params
